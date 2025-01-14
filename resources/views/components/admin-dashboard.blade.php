92b42e6ce442d99fcae52678be97c54a1caab561
Jesper <49792683+Repsej07@users.noreply.github.com>
2025-01-14 17:51:31 +0100

    admin-dash changes

--- resources/views/components/admin-dashboard.blade.php
+++ resources/views/components/admin-dashboard.blade.php
@@ -1,3 +1,7 @@
-<div class="min-w-80 min-h-fit bg-black">
-
+<div class="flex flex-row max-w-1/2 mt-10 justify-center">
+<div class="flex flex-row w-1/2 h-144 ">
+<div class="w-80 h-144 bg-black"></div>
+<div class="w-80 h-144 bg-blue-950"></div>
+<div class="w-80 h-144 bg-pink-200"></div>
+</div>
 </div>
