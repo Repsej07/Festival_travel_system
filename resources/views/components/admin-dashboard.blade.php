f88220f0e382fccb8c19dffe157671070041686c
Jesper <49792683+Repsej07@users.noreply.github.com>
2025-01-15 14:52:53 +0100

    spacing on the different dashes

--- resources/views/components/admin-dashboard.blade.php
+++ resources/views/components/admin-dashboard.blade.php
@@ -1,5 +1,5 @@
-<div class="flex flex-row max-w-1/2 mt-10 justify-center">
-<div class="flex flex-row w-1/2 h-144 ">
+<div class="flex flex-row max-w-screen mt-10 justify-center">
+<div class="flex flex-row w-[80vw] h-144 justify-between">
 <div class="w-80 h-144 bg-black"></div>
 <div class="w-80 h-144 bg-blue-950"></div>
 <div class="w-80 h-144 bg-pink-200"></div>
