63b3db0bf942ea20bf0125a34e5266dbe612a3c3
Jesper <49792683+Repsej07@users.noreply.github.com>
2025-01-29 00:14:47 +0100

    I have fixed the insecurity in my web.php

--- routes/web.php
+++ routes/web.php
@@ -47,34 +47,33 @@ Route::post('/contact', [ContactController::class, 'store'])->name('contact');
 
 
 // Admin-specific resource routes
-Route::resource('admin', AdminController::class)
-    ->only(['index', 'store', 'searchusers', 'searchFestivals'])
-    ->middleware(['auth', 'verified', 'admin']);
-Route::get('/admin/searchusers', [AdminController::class, 'searchUsers'])->name('admin.searchusers');
-Route::get('/admin/searchfestivals', [AdminController::class, 'searchFestivals'])->name('admin.searchfestivals');
-Route::get('/admin/searchbusreizen', [AdminController::class, 'searchBusreizen'])->name('admin.searchbusreizen');
-// Admin-specific festival routes
-Route::get('/admin/festivals/create', [AdminController::class, 'createFestival'])->name('admin.festivals.create');
-Route::post('/admin/festivals/store', [AdminController::class, 'storeFestival'])->name('admin.festivals.store');
-Route::get('/admin/festivals/{festival}/edit', [AdminController::class, 'editFestival'])->name('admin.festival.edit');
-Route::patch('/admin/festivals/{festival}', [AdminController::class, 'updateFestival'])->name('admin.festival.update');
-Route::delete('/admin/festivals/{festival}', [AdminController::class, 'destroyFestival'])->name('admin.festival.delete');
-Route::get('/admin/festivals/info/{festival}', [AdminController::class, 'showFestival'])->name('admin.festival.info');
+Route::prefix('admin')->middleware(['auth', 'verified', 'admin'])->group(function () {
+    Route::resource('/', AdminController::class)->except(['index', 'store']);
+    Route::get('/admin/searchusers', [AdminController::class, 'searchUsers'])->name('admin.searchusers');
+    Route::get('/admin/searchfestivals', [AdminController::class, 'searchFestivals'])->name('admin.searchfestivals');
+    Route::get('/admin/searchbusreizen', [AdminController::class, 'searchBusreizen'])->name('admin.searchbusreizen');
+    // Admin-specific festival routes
+    Route::get('/admin/festivals/create', [AdminController::class, 'createFestival'])->name('admin.festivals.create');
+    Route::post('/admin/festivals/store', [AdminController::class, 'storeFestival'])->name('admin.festivals.store');
+    Route::get('/admin/festivals/{festival}/edit', [AdminController::class, 'editFestival'])->name('admin.festival.edit');
+    Route::patch('/admin/festivals/{festival}', [AdminController::class, 'updateFestival'])->name('admin.festival.update');
+    Route::delete('/admin/festivals/{festival}', [AdminController::class, 'destroyFestival'])->name('admin.festival.delete');
+    Route::get('/admin/festivals/info/{festival}', [AdminController::class, 'showFestival'])->name('admin.festival.info');
 
-// Admin-specific busreizen routes
-Route::get('/admin/busreizen/create', [AdminController::class, 'createBusreis'])->name('admin.busreizen.create');
-Route::post('/admin/busreizen/store', [AdminController::class, 'storeBusreis'])->name('admin.busreizen.store');
-Route::get('/admin/busreizen/{busreis}/edit', [AdminController::class, 'editBusreis'])->name('admin.busreis.edit');
-Route::patch('/admin/busreizen/{busreis}', [AdminController::class, 'updateBusreis'])->name('admin.busreis.update');
-Route::delete('/admin/busreizen/{busreis}', [AdminController::class, 'destroyBusreis'])->name('admin.busreis.delete');
-Route::get('/admin/busreizen/info/{busreis}', [AdminController::class, 'showBusreis'])->name('admin.busreis.info');
-// Admin-specific user routes
-Route::get('/admin/user/create', [AdminController::class, 'createUser'])->name('admin.user.create');
-Route::get('/admin/user/{user}/edit', [AdminController::class, 'editUser'])->name('admin.user.edit');
-Route::patch('/admin/user/{user}', [AdminController::class, 'updateUser'])->name('admin.user.update');
-Route::post('/admin/user/store', [AdminController::class, 'storeUser'])->name('admin.user.store');
-Route::delete('/admin/user/{user}', [AdminController::class, 'destroyUser'])->name('admin.user.delete');
-Route::get('/admin/user/info/{user}', [AdminController::class, 'showUser'])->name('admin.user.info');
-
+    // Admin-specific busreizen routes
+    Route::get('/admin/busreizen/create', [AdminController::class, 'createBusreis'])->name('admin.busreizen.create');
+    Route::post('/admin/busreizen/store', [AdminController::class, 'storeBusreis'])->name('admin.busreizen.store');
+    Route::get('/admin/busreizen/{busreis}/edit', [AdminController::class, 'editBusreis'])->name('admin.busreis.edit');
+    Route::patch('/admin/busreizen/{busreis}', [AdminController::class, 'updateBusreis'])->name('admin.busreis.update');
+    Route::delete('/admin/busreizen/{busreis}', [AdminController::class, 'destroyBusreis'])->name('admin.busreis.delete');
+    Route::get('/admin/busreizen/info/{busreis}', [AdminController::class, 'showBusreis'])->name('admin.busreis.info');
+    // Admin-specific user routes
+    Route::get('/admin/user/create', [AdminController::class, 'createUser'])->name('admin.user.create');
+    Route::get('/admin/user/{user}/edit', [AdminController::class, 'editUser'])->name('admin.user.edit');
+    Route::patch('/admin/user/{user}', [AdminController::class, 'updateUser'])->name('admin.user.update');
+    Route::post('/admin/user/store', [AdminController::class, 'storeUser'])->name('admin.user.store');
+    Route::delete('/admin/user/{user}', [AdminController::class, 'destroyUser'])->name('admin.user.delete');
+    Route::get('/admin/user/info/{user}', [AdminController::class, 'showUser'])->name('admin.user.info');
+});
 // routes/web.php
 require __DIR__ . '/auth.php';
