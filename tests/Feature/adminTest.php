f8e52d262f1c136d81f486b643f640940f0c60ec
Jesper <49792683+Repsej07@users.noreply.github.com>
2025-03-31 14:10:23 +0200

    Wrote the admin test, to test if a guest cant access the admin dashboard, and to test if a  admin can access it.

--- tests/Feature/adminTest.php
+++ tests/Feature/adminTest.php
@@ -5,16 +5,33 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class AdminTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+
+    public function test_admin_can_access_dashboard()
     {
-        $response = $this->get('/');
+        $admin = User::factory()->create([
+            'admin' => 1,
+        ]);
+
+        $this->actingAs($admin);
+
+        $response = $this->get('/admin/');
 
         $response->assertStatus(200);
+    }
+    public function test_user_cant_access_admin_dashboard(){
+
+        $user = User::factory()->create([
+            'admin' => 0,
+        ]);
+
+        $this->actingAs($user);
+
+        $response = $this->get('/admin/');
+
+        $response->assertStatus(403);
     }
 }
